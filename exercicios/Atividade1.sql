1878219854249fd85406ea3830284e8506da469c
Vitória Manuela <contatodavitoria@gmail.com>
2024-11-22 13:15:00 -0300

    Atividade1.sql

--- exercicios/Atividade1.sql
+++ exercicios/Atividade1.sql
@@ -3,18 +3,18 @@ CREATE DATABASE db_colaboradores;
 USE db_colaboradores;
 
 CREATE TABLE tb_colaboradores (
-Matricula BIGINT AUTO_INCREMENT,
-Nome VARCHAR (255),
-data_nascimento DATE,
+Matricula BIGINT AUTO_INCREMENT NOT NULL,
+Nome VARCHAR (255) NOT NULL,
+data_nascimento DATE NOT NULL,
 Email VARCHAR (255) UNIQUE,
-Cargo VARCHAR (255),
-Salario DECIMAL (10, 2),
+Cargo VARCHAR (255) NOT NULL,
+Salario DECIMAL (10, 2) NOT NULL,
 PRIMARY KEY (Matricula)
 );
 
 INSERT INTO tb_colaboradores (Nome, data_nascimento, Email, Cargo, Salario)
 VALUES
-("Vitória Manuela", "2003-11-10", "vitoriamanuela@gmail.com", "Desevolvedora Junior", 3000.00),
+("Vitória Manuela", "2003-11-10", "vitoriamanuela@gmail.com", "Desenvolvedora Junior", 3000.00),
 ("Dayanne Santos", "2000-02-13", "dayannesantos@gmail.com", "Assistente de Produção", 2500.00),
 ("Lucas Brito", "1995-04-20", "lucasbrito@gmail.com", "Assistente Administrativo", 1700.00),
 ("Ana Beatriz", "2004-02-17", "anabeatriz@gmail.com", "Assistente de Departamento", 2500.00),
